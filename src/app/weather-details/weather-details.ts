29cd954fba467a0f6d954d513038fe0ba48bc70d
Dawid Mackiewicz <dawid2004m@gmail.com>
2026-05-15 23:48:47 +0200

    styles

--- src/app/weather-details/weather-details.ts
+++ src/app/weather-details/weather-details.ts
@@ -1,24 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { WeatherInterface } from '../weather-interface';
 
 @Component({
   selector: 'app-weather-details',
   imports: [],
   template: `
     <div>
-      <h1> Temp </h1>
-      <h2> City </h2>
-      <h3> Weather description </h3>
-      <div class="weather details">
-        <div class="huminty">
-          <h2>LVL</h2>
-          <h3>Huminty</h3>
+      <div class="weather-main-info">
+        <h1> {{ weatherDetails.temperature }}° </h1>
+        <h2> {{ weatherDetails.city }}  , {{ weatherDetails.country }}</h2>
+        <h3> Weather description </h3>
+      </div>
+      <div class="weather-details">
+        <div>
+          <h2>{{ weatherDetails.humanidity}}%</h2>
+          <h3>Humanidity</h3>
         </div>
-        <div class="wind">
-          <h2>LVL</h2>
+        <div>
+          <h2>{{ weatherDetails.wind }} km/h</h2>
           <h3>Wind</h3>
         </div>
-        <div class="presure">
-          <h2>LVL</h2>
+        <div>
+          <h2>{{ weatherDetails.pressure }} hPa</h2>
           <h3>Presure</h3>
         </div>
       </div>
@@ -26,4 +29,20 @@ import { Component } from '@angular/core';
   `,
   styleUrl: './weather-details.css',
 })
-export class WeatherDetails {}
+export class WeatherDetails {
+  @Input() weatherDetails!: WeatherInterface;
+
+  constructor(){
+     const weatherMocup: WeatherInterface = {
+      city: "New York",
+      country: "USA",
+      temperature: 32,
+      humanidity: 62,
+      wind: 32,
+      pressure: 10045,
+      cloud: 24,
+      condition: "sunny"
+     }
+     this.weatherDetails = weatherMocup;
+    }
+}
---
+++ src/app/weather-interface.ts
@@ -0,0 +1,10 @@
+export interface WeatherInterface {
+    city : string;
+    country: string;
+    temperature: number;
+    humanidity: number;
+    wind: number;
+    pressure: number;
+    cloud: number;
+    condition: string;
+}
